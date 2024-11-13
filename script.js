22e83a3afb0388a6b96e534b2231ecea24995319
FranquitoAuris <franquitoauris@gmail.com>
2024-11-13 20:21:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -65,3 +65,23 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     }
   });
   
+
+
+
+
+
+  function toggleForms(event) {
+    event.preventDefault(); // Esto evitará que la página se desplace
+  
+    const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('register-form');
+  
+    if (loginForm.style.display === 'none') {
+      loginForm.style.display = 'block';
+      registerForm.style.display = 'none';
+    } else {
+      loginForm.style.display = 'none';
+      registerForm.style.display = 'block';
+    }
+  }
+  
